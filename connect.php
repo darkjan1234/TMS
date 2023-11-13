ccaa7af31491e1111ac797307081f6319cdda5a9
Shakaman <darkjan1234>
2023-11-14 02:47:48 +0800

    waaaaaahhh?

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@ $db_pass		= '';
 $db_database	= 'tos'; 
 
 /* End config */
+/*yaya */
 
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
