0ffd5d7e53cb7157172bcf91964fcccd5d9636cd
Shakaman <darkjan1234>
2023-11-19 12:18:38 +0800

    hahahah pakbung bung

--- INSTALL/DATABASE.sql
+++ INSTALL/DATABASE.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2018 at 08:32 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Nov 19, 2023 at 05:17 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `tos`
@@ -26,10 +27,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `offence`
 --
 
-CREATE TABLE IF NOT EXISTS `offence` (
+CREATE TABLE `offence` (
   `id` int(11) NOT NULL,
   `offence` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -37,25 +38,30 @@ CREATE TABLE IF NOT EXISTS `offence` (
 -- Table structure for table `reported_offence`
 --
 
-CREATE TABLE IF NOT EXISTS `reported_offence` (
+CREATE TABLE `reported_offence` (
   `id` int(11) NOT NULL,
   `offence_id` varchar(200) NOT NULL,
   `vehicle_no` varchar(200) NOT NULL,
   `driver_license` varchar(300) NOT NULL,
   `name` varchar(500) NOT NULL,
   `address` varchar(1000) NOT NULL,
+  `contact` int(200) NOT NULL,
+  `fines` int(200) NOT NULL,
   `gender` varchar(300) NOT NULL,
   `officer_reporting` varchar(500) NOT NULL,
   `offence` varchar(1000) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `reported_offence`
 --
 
-INSERT INTO `reported_offence` (`id`, `offence_id`, `vehicle_no`, `driver_license`, `name`, `address`, `gender`, `officer_reporting`, `offence`, `date`) VALUES
-(1, '78771c', '08954345355', '467545635', 'Adisa Adetobi', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 'Male', 'Trorrahclef', 'Driving Under Drug Influence', '2018-05-28');
+INSERT INTO `reported_offence` (`id`, `offence_id`, `vehicle_no`, `driver_license`, `name`, `address`, `contact`, `fines`, `gender`, `officer_reporting`, `offence`, `date`) VALUES
+(1, '78771c', '08954345355', '467545635', 'Adisa Adetobi', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 0, 0, 'Male', 'Trorrahclef', 'Driving Under Drug Influence', '2018-05-28'),
+(2, '87c562', '5346346', '24625624', 'Janritz Cabansag', 'prk7newsociety', 0, 0, 'Male', '1234', 'Underage driving', '2023-11-14'),
+(3, '78771c', '08954345355', '467545635', 'Adisa Adetobi', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 0, 0, 'Male', 'Trorrahclef', 'Driving Under Drug Influence', '2018-05-28'),
+(4, 'b45395', '5346346', '35235235', 'Janritz Cabansag', 'prk7newsociety', 0, 0, 'Male', '1234', 'Speed Limit violation', '2023-11-14');
 
 -- --------------------------------------------------------
 
@@ -63,7 +69,7 @@ INSERT INTO `reported_offence` (`id`, `offence_id`, `vehicle_no`, `driver_licens
 -- Table structure for table `site_settings`
 --
 
-CREATE TABLE IF NOT EXISTS `site_settings` (
+CREATE TABLE `site_settings` (
   `id` int(11) NOT NULL,
   `site_name` varchar(300) NOT NULL,
   `site_desc` varchar(2000) NOT NULL,
@@ -72,14 +78,14 @@ CREATE TABLE IF NOT EXISTS `site_settings` (
   `address` varchar(500) NOT NULL,
   `city` varchar(300) NOT NULL,
   `country` varchar(300) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `site_settings`
 --
 
 INSERT INTO `site_settings` (`id`, `site_name`, `site_desc`, `email`, `phone`, `address`, `city`, `country`) VALUES
-(1, 'Trafity', 'Welcome to Trafity Dashboard - a beautiful Traffic Offence System.', 'admin@we.com', '+2348138652645', 'Alagbaka GRA', 'Lagos', 'Nigeria');
+(1, 'Traffic Management System', 'Traffic Management System', 'admin@we.com', '0923523522', 'apopong', 'Gensan', 'Philippines');
 
 -- --------------------------------------------------------
 
@@ -87,7 +93,7 @@ INSERT INTO `site_settings` (`id`, `site_name`, `site_desc`, `email`, `phone`, `
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `user_id` varchar(200) NOT NULL,
   `name` varchar(500) NOT NULL,
@@ -96,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(300) NOT NULL,
   `address` varchar(500) NOT NULL,
   `position` varchar(300) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
@@ -105,7 +111,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `user_id`, `name`, `username`, `pass`, `email`, `address`, `position`) VALUES
 (1, 'bcefa114ee', 'Adeyemi   Femipo', 'Torrahclef', 'yemiyemi', 'awolu_faith@live.com', 'Alagbaka, Akure, Ondo Nigeria', 'admin'),
 (4, '0fd73c73c1', 'Full Name', 'Olapade', 'uhsfr', 'ayomi@we.com', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 'admin'),
-(5, '5dea1fd9c3', 'Awolu Faith', 'tobi', 'tobi', 'ayomi@we.com', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 'officer');
+(5, '5dea1fd9c3', 'Awolu Faith', 'tobi', 'tobi', 'ayomi@we.com', 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09', 'officer'),
+(8, '642cb035d1', 'Janritz Cabansag', '1234', '1234', 'janritzcabansag123@gmail.com', 'prk7newsociety', 'admin'),
+(9, '91f568ab52', 'Janritz Cabansag', '12345', '12345', 'janritzcabansag123@gmail.com', 'prk7newsociety', 'officer');
 
 --
 -- Indexes for dumped tables
@@ -137,17 +145,21 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `reported_offence`
 --
 ALTER TABLE `reported_offence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `site_settings`
 --
 ALTER TABLE `site_settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
